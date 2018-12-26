7f4f5ad5a5ee372f601a6183f6945ea7135f776c
OnlyCareful <fuzhuang9308@163.com>
2018-12-26 21:36:10 +0800

    Initial commit

---
+++ src/main/java/com/chatweb/only/OnlyApplication.java
@@ -0,0 +1,14 @@
+package com.chatweb.only;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyApplication.class, args);
+    }
+
+}
+
