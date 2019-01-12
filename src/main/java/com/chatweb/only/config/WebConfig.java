45731f113d381fbb9ec6712a8250077970a83682
OnlyCareful <fuzhuang9308@163.com>
2019-01-12 22:42:00 +0800

    拦截器、返回值格式统一处理配置

--- src/main/java/com/chatweb/only/config/WebConfig.java
+++ src/main/java/com/chatweb/only/config/WebConfig.java
@@ -1,4 +1,82 @@
 package com.chatweb.only.config;
 
-public class WebConfig {
+import com.chatweb.only.bean.Result;
+import com.chatweb.only.bean.ResultCode;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器配置
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * 加载拦截器（拦截器的执行是会根据 registry 注入的先后顺序执行）
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/test");
+    }
+
+    /**
+     * 拦截器
+     */
+    private class WebInterceptor implements HandlerInterceptor {
+        /**
+         * 请求处理前拦截
+         */
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            System.out.println("请求处理前拦截");
+            return true;
+        }
+
+        /**
+         * 请求处理后，视图渲染前
+         */
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+            System.out.println("请求处理后，视图渲染前");
+        }
+
+        /**
+         * 整个请求结束后
+         */
+        @Override
+        public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+            System.out.println("整个请求结束后");
+        }
+    }
+}
+
+/**
+ * 返回值格式统一处理配置
+ */
+@ControllerAdvice
+class ReturnedFormatConfig implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return !methodParameter.getMethod().getReturnType().isInstance(Result.class);
+    }
+
+    @Override
+    public Result beforeBodyWrite(Object returnValue, MethodParameter methodParameter, MediaType mediaType,
+                                  Class<? extends HttpMessageConverter<?>> aClass,
+                                  ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return new Result<>(ResultCode.SUCCEED,returnValue);
+    }
 }
